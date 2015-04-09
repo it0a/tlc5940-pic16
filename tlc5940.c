9cfb4b60bad1d7f8327114552815ca3e66978775
tay <taylor26892@gmail.com>
2015-04-09 19:18:40 -0400

    Slight cleanup

--- tlc5940.c
+++ tlc5940.c
@@ -10,11 +10,73 @@ void TLC5940_Init(void) {
     __TLC_VPRG_DIR = __TLC_OUTPUT;
     __TLC_XLAT_DIR = __TLC_OUTPUT;
     // Initial values from programming flow chart
-    __TLC_BLANK =__TLC_HIGH;
-    __TLC_GSCLK =__TLC_LOW;
-    __TLC_SCLK =__TLC_LOW;
-    __TLC_SIN =__TLC_LOW;
-    __TLC_VPRG =__TLC_HIGH;
-    __TLC_XLAT =__TLC_LOW;
+    __TLC_BLANK = __TLC_HIGH;
+    __TLC_GSCLK = __TLC_LOW;
+    __TLC_SCLK = __TLC_LOW;
+    __TLC_SIN = __TLC_LOW;
+    __TLC_VPRG = __TLC_HIGH;
+    __TLC_XLAT = __TLC_LOW;
+    //
+    TLC5940_ClockInDC();
+}
+
+void TLC5940_ClockInDC(void) {
+    unsigned char counter = 0;
+    __TLC_SIN = __TLC_HIGH;
+    __TLC_VPRG = __TLC_HIGH;
+    for(;;) {
+        if (counter > __TLC_DC_COUNTER_MAX) {
+            __TLC_XLAT = __TLC_HIGH;
+            __TLC_XLAT = __TLC_LOW;
+            break;
+        } else {
+            if (dcData[counter]) {
+                __TLC_SIN = __TLC_HIGH;
+            } else {
+                __TLC_SIN = __TLC_LOW;
+            }
+            __TLC_SCLK = __TLC_HIGH;
+            __TLC_SCLK = __TLC_LOW;
+            counter++;
+        }
+    }
+}
+
+void TLC5940_SetGS_GW_PWM(void) {
+    unsigned char firstCycleFlag = 0;
+    unsigned short GSCLK_Counter = 0;
+    unsigned char Data_Counter = 0;
+    if (__TLC_VPRG == __TLC_HIGH) {
+        __TLC_VPRG = __TLC_LOW;
+        firstCycleFlag = 1;
+    }
+    __TLC_BLANK = __TLC_LOW;
+    for (;;) {
+        if (GSCLK_Counter > __TLC_GSCLK_COUNTER_MAX) {
+            __TLC_BLANK = __TLC_HIGH;
+            __TLC_XLAT = __TLC_HIGH;
+            __TLC_XLAT = __TLC_LOW;
+            if (firstCycleFlag) {
+                __TLC_SCLK = __TLC_HIGH;
+                __TLC_SCLK = __TLC_LOW;
+                firstCycleFlag = 0;
+            }
+            break;
+        } else {
+            if (!(Data_Counter > __TLC_DATA_COUNTER_MAX)) {
+                if (1) {
+                    __TLC_SIN = __TLC_HIGH;
+                } else {
+                    __TLC_SIN = __TLC_LOW;
+                }
+                __TLC_SCLK = __TLC_HIGH;
+                __TLC_SCLK = __TLC_LOW;
+                Data_Counter++;
+            }
+        }
+        __TLC_GSCLK = __TLC_HIGH;
+        __TLC_GSCLK = __TLC_LOW;
+        GSCLK_Counter++;
+    }
 }
 
--- tlc5940.h
+++ tlc5940.h
@@ -8,6 +8,8 @@
 #define TLC5940_N 1
 #endif
 
+#define __TLC_LED_N 16 * TLC5940_N
+
 // GSCLK requires CCP1.
 // CCP1 maps to RC2 (See datasheet)
 // GSCLK must be mapped to RC2.
@@ -46,6 +48,65 @@
 #define __TLC_LOW 0
 #define __TLC_HIGH 1
 
+#if (12 * TLC5940_N > 255)
+#define dcData_t unsigned short
+#else
+#define dcData_t unsigned char
+#endif
+
+#if (24 * TLC5940_N > 255)
+#define gsData_t unsigned short
+#else
+#define gsData_t unsigned char
+#endif
+
+#define __TLC_DC_SIZE 96
+#define __TLC_GS_SIZE 192
+#define __TLC_DC_COUNTER_MAX (__TLC_DC_SIZE * TLC5940_N - 1)
+#define __TLC_DATA_COUNTER_MAX (__TLC_GS_SIZE * TLC5940_N - 1)
+#define __TLC_GSCLK_COUNTER_MAX 4095
+
+/// MSB--------->LSB
+unsigned char dcData[__TLC_DC_SIZE * TLC5940_N] = {
+    1, 1, 1, 1, 1, 1, // Channel 15
+    1, 1, 1, 1, 1, 1, // Channel 14
+    1, 1, 1, 1, 1, 1, // Channel 13
+    1, 1, 1, 1, 1, 1, // Channel 12
+    1, 1, 1, 1, 1, 1, // Channel 11
+    1, 1, 1, 1, 1, 1, // Channel 10
+    1, 1, 1, 1, 1, 1, // Channel 9
+    1, 1, 1, 1, 1, 1, // Channel 8
+    1, 1, 1, 1, 1, 1, // Channel 7
+    1, 1, 1, 1, 1, 1, // Channel 6
+    1, 1, 1, 1, 1, 1, // Channel 5
+    1, 1, 1, 1, 1, 1, // Channel 4
+    1, 1, 1, 1, 1, 1, // Channel 3
+    1, 1, 1, 1, 1, 1, // Channel 2
+    1, 1, 1, 1, 1, 1, // Channel 1
+    1, 1, 1, 1, 1, 1  // Channel 0
+};
+
+//unsigned char gsData[__TLC_GS_SIZE * TLC5940_N] = {
+    //0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Channel 15
+    //0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Channel 14
+    //0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Channel 13
+    //0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, // Channel 12
+    //0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, // Channel 11
+    //0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, // Channel 10
+    //0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, // Channel 9
+    //0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, // Channel 8
+    //0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, // Channel 7
+    //0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, // Channel 6
+    //0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, // Channel 5
+    //0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, // Channel 4
+    //0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Channel 3
+    //0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Channel 2
+    //1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // Channel 1
+    //1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1  // Channel 0
+//};
+
 void TLC5940_Init(void);
+void TLC5940_ClockInDC(void);
+void TLC5940_SetGS_GW_PWM(void);
 
 #endif
