713a6357de1767a522f4cf0545ba2a5245f07157
tay <taylor26892@gmail.com>
2015-04-09 17:22:34 -0400

    Adding working configuration

--- tlc5940.c
+++ tlc5940.c
@@ -1,6 +1,20 @@
-#include <pic16f877.h>
+#include <xc.h>
 #include "tlc5940.h"
 
 void TLC5940_Init(void) {
-    //
+    // Set as outputs
+    __TLC_BLANK_DIR = __TLC_OUTPUT;
+    __TLC_GSCLK_DIR = __TLC_OUTPUT;
+    __TLC_SCLK_DIR = __TLC_OUTPUT;
+    __TLC_SIN_DIR = __TLC_OUTPUT;
+    __TLC_VPRG_DIR = __TLC_OUTPUT;
+    __TLC_XLAT_DIR = __TLC_OUTPUT;
+    // Initial values from programming flow chart
+    __TLC_BLANK =__TLC_HIGH;
+    __TLC_GSCLK =__TLC_LOW;
+    __TLC_SCLK =__TLC_LOW;
+    __TLC_SIN =__TLC_LOW;
+    __TLC_VPRG =__TLC_HIGH;
+    __TLC_XLAT =__TLC_LOW;
 }
+
--- tlc5940.h
+++ tlc5940.h
@@ -1,34 +1,50 @@
 #ifndef TLC5940_H
 #define TLC5940_H
 
-#include <pic16f877.h>
+#include <xc.h>
+
+// Number of TLC5940s to be used (default: 1)
+#ifndef TLC5940_N
+#define TLC5940_N 1
+#endif
 
 // GSCLK requires CCP1.
 // CCP1 maps to RC2 (See datasheet)
 // GSCLK must be mapped to RC2.
-#define GSCLK_PORT  PORTCbits.RC2
+#define __TLC_GSCLK_DIR  TRISC2
+#define __TLC_GSCLK      RC2
 
 // SCLK requires SCK
 // SCK maps to RC3 (See datasheet)
 // SCLK must be mapped to RC3.
-#define SCLK_PORT   PORTCbits.RC3
+#define __TLC_SCLK_DIR   TRISC3
+#define __TLC_SCLK       RC3
 
 // SIN requires SDO
 // SDO maps to RC5 (See datasheet)
 // SIN must be mapped to RC5.
-#define SIN_PORT    PORTCbits.RC5
+#define __TLC_SIN_DIR    TRISC5
+#define __TLC_SIN        RC5
 
 // XLAT has no requirements.
 // XLAT can be remapped if necessary.
-#define XLAT_PORT	PORTAbits.RA0
+#define __TLC_XLAT_DIR	TRISB0
+#define __TLC_XLAT	    RB0
 
 // BLANK has no requirements.
 // BLANK can be remapped if necessary.
-#define BLANK_PORT	PORTAbits.RA1
+#define __TLC_BLANK_DIR	TRISB1
+#define __TLC_BLANK	    RB1
 
 // VPRG has no requirements.
 // VPRG can be remapped if necessary.
-#define VPRG_PORT	PORTAbits.RA2
+#define __TLC_VPRG_DIR	TRISB2
+#define __TLC_VPRG	    RB2
+
+#define __TLC_OUTPUT 0
+#define __TLC_INPUT 1
+#define __TLC_LOW 0
+#define __TLC_HIGH 1
 
 void TLC5940_Init(void);
 
